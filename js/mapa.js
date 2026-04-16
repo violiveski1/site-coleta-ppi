bf82b8f04c8e0fe8752083607936e755e953d65b
violiveski1 <vicenteoliveski@gmail.com>
2026-04-16 12:51:44 -0300

    limitamos o mapa

--- js/mapa.js
+++ js/mapa.js
@@ -1,13 +1,23 @@
-// Coordenadas de São Borja - RS
-var map = L.map('map').setView([-28.6606, -56.0044], 13);
+var limites = [
+    [-28.75, -56.10],
+    [-28.60, -55.90]
+];
 
-// Camada do mapa
+// cria o mapa já dentro dos limites
+var map = L.map('map', {
+    maxBounds: limites,
+    maxBoundsViscosity: 1.0,
+    minZoom: 12,
+    maxZoom: 16
+}).setView([-28.6606, -56.0044], 13);
+
+// camada do mapa
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
 
-// Marcador em São Borja
+// marcador
 L.marker([-28.6606, -56.0044])
     .addTo(map)
-    .bindPopup("<b>São Borja</b><br>Ponto de coleta eletrônico")
-    .openPopup();
+    .bindPopup("São Borja - Ponto de coleta");
+
