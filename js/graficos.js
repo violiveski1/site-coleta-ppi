516968acf9585557fcbb0c74d07a8763150d503f
violiveski1 <vicenteoliveski@gmail.com>
2026-05-20 11:01:13 -0300

    Funcionalidade: Adicionada geração de gráficos para respostas de pesquisa sobre lixo eletrônico
    - Implementada a coleta de dados da API do Google Sheets para as respostas da pesquisa.
    
    - Criados gráficos de pizza para diversas perguntas da pesquisa: conhecimento sobre pontos de coleta, compreensão sobre lixo eletrônico, consciência das consequências do descarte inadequado e armazenamento pessoal de lixo eletrônico.
    
    - Adicionada uma função para atualizar os dados a cada 10 segundos.

---
+++ js/graficos.js
@@ -0,0 +1,111 @@
+const URL =
+"https://opensheet.elk.sh/1tAbdoMPOTdl8jQSV_LiYbKZmggIxAkTvfv7fpcIDOwQ/Respostas%20ao%20formul%C3%A1rio%201";
+
+async function carregarGraficos() {
+
+    const response = await fetch(URL);
+    const dados = await response.json();
+
+    console.log(dados);
+
+    const tipos = {};
+    const conhecimento = {};
+    const consequencias = {};
+    const guardam = {};
+
+    dados.forEach(item => {
+
+        const tipo =
+        item["Você conhece os pontos de coleta de lixo eletrônico da sua cidade?"];
+
+        if(tipo){
+
+            tipos[tipo] =
+            (tipos[tipo] || 0) + 1;
+
+        }
+
+        const respostaConhecimento =
+        item["O quanto você sabe sobre Lixo Eletrônico?"];
+
+        if(respostaConhecimento){
+
+            conhecimento[respostaConhecimento] =
+            (conhecimento[respostaConhecimento] || 0) + 1;
+
+        }
+
+        const respostaConsequencias =
+        item["Você sabe as consequências do descarte incorreto de lixo eletrônico?"];
+
+        if(respostaConsequencias){
+
+            consequencias[respostaConsequencias] =
+            (consequencias[respostaConsequencias] || 0) + 1;
+
+        }
+
+        const respostaGuardam =
+        item["Você guarda lixo eletrônico em casa?"];
+
+        if(respostaGuardam){
+
+            guardam[respostaGuardam] =
+            (guardam[respostaGuardam] || 0) + 1;
+
+        }
+
+    });
+
+    criarGraficoPizza(
+        "graficoTipos",
+        tipos
+    );
+
+    criarGraficoPizza(
+        "graficoConhecimento",
+        conhecimento
+    );
+
+    criarGraficoPizza(
+        "graficoConsequencias",
+        consequencias
+    );
+
+    criarGraficoPizza(
+        "graficoGuardam",
+        guardam
+    );
+
+}
+
+// =========================
+// FUNÇÃO PIZZA
+// =========================
+
+function criarGraficoPizza(id, dados){
+
+    new Chart(
+        document.getElementById(id),
+        {
+            type: "pie",
+
+            data: {
+                labels: Object.keys(dados),
+
+                datasets: [{
+                    data: Object.values(dados)
+                }]
+            }
+        }
+    );
+
+}
+
+carregarGraficos();
+
+setInterval(() => {
+
+    location.reload();
+
+}, 10000);
